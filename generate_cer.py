7e2e460f34bd9edbcd7c7ee6bef9ecc5e496a8c8
Orlind Bajraktari <orlind.bajraktari@student.uni-pr.edu>
2026-05-13 14:56:12 +0200

    Update generate_cer.py

---
+++ generate_cer.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
+
+
+BASE_DIR = Path(__file__).resolve().parent
+CERT_DIR = BASE_DIR / "certificates"
